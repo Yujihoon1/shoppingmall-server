fd4b461eb2694e4f31eb22cbfad1ab21f071ce1b
Yujihoon1 <yjh150062@naver.com>
2024-06-06 13:49:11 +0900

    server
    1. promotion의 활성화 여부를 server에서 관리하는 것 추가
       1-1) 현재 날짜가 프로모션 기간내에 있는 지 체크 + 조건에 맞을시 활성화
       1-2) 만료된 프로모션은 비활성화

--- server.js
+++ server.js
@@ -1,22 +1,44 @@
+require("dotenv").config();
+
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const db = require("./db");
+const cors = require("cors");
+const session = require("express-session");
 const User = require("./Models/User");
 const userRoute = require("./routes/userRoute");
 const loginRoute = require("./routes/loginRoute");
-const cors = require("cors");
-const session = require("express-session");
+const paymentsRoute = require("./routes/paymentsRoute");
+const myinfoRoute = require("./routes/myinfoRoute");
+const orderRoute = require("./routes/orderRoute");
+const mypageRoute = require("./routes/mypageRoute");
+const productRoute = require("./routes/productRoute");
+const promotionRoute = require("./routes/promotionManagementRoute");
+const categoryRoute = require("./routes/categoryRoute");
+const cartRoute = require("./routes/cartRoute");
+const Promotion = require("./Models/Promotions");
 
-app.use(cors());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-require("dotenv").config();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      sameSite: "none",
+      secure: false, // 개발 환경에서는 false, 배포 환경에서는 true
+    },
   })
 );
 
@@ -24,6 +46,14 @@ app.use(express.json()); // JSON 파싱 미들웨어
 
 app.use("/users", userRoute); // '/users' 경로로 들어오는 모든 요청을 userRoute에서 처리
 app.use("/login", loginRoute); // '/login' 경로로 들어오는 모든 요청을 loginRoute에서 처리
+app.use("/payments", paymentsRoute);
+app.use("/myinfos", myinfoRoute);
+app.use("/orders", orderRoute);
+app.use("/mypage", mypageRoute);
+app.use("/products", productRoute);
+app.use("/promotions", promotionRoute);
+app.use("/category", categoryRoute);
+app.use("/cart", cartRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
@@ -44,4 +74,45 @@ db.authenticate()
 app.post("/users", async (req, res) => {
   const newUser = await User.create(req.body);
   res.json(newUser);
+});
+
+const cron = require("node-cron");
+const { Op } = require("sequelize");
+
+// 매일 자정에 실행되는 작업 스케줄링
+cron.schedule("0 0 * * *", async () => {
+  const today = new Date();
+  try {
+    // 현재 날짜가 프로모션 기간 내에 있는지 체크하고, 상태 업데이트
+    // 활성화 조건에 맞는 프로모션을 활성화
+    await Promotion.update(
+      { promotion_is_visible: true },
+      {
+        where: {
+          promotion_start_date: {
+            [Op.lte]: today,
+          },
+          promotion_end_date: {
+            [Op.gte]: today,
+          },
+          promotion_is_visible: false, // 기존에 비활성화된 프로모션만 대상으로 합니다.
+        },
+      }
+    );
+    // 만료된 프로모션을 비활성화
+    await Promotion.update(
+      { promotion_is_visible: false },
+      {
+        where: {
+          [Op.or]: [
+            { promotion_start_date: { [Op.gt]: today } },
+            { promotion_end_date: { [Op.lt]: today } },
+          ],
+          promotion_is_visible: true, // 기존에 활성화된 프로모션만 대상으로 합니다.
+        },
+      }
+    );
+  } catch (error) {
+    console.error("Error updating promotion visibility:", error);
+  }
 });
